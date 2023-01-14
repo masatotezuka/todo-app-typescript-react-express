6976a34482acbe260783e34cf150f7f9e77519d7
masatotezuka <tez.0731.mst@gmail.com>
2023-01-14 12:03:55 +0900

    add: StyledToastContainer

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,14 +5,28 @@ import { store } from "./store";
 import App from "./App";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const container = document.getElementById("root")!;
 const root = createRoot(container);
+
+export const StyledToastContainer = () => {
+  return (
+    <ToastContainer
+      position="top-left"
+      autoClose={4000}
+      hideProgressBar={true}
+      style={{ width: "450px", fontSize: "14px" }}
+    ></ToastContainer>
+  );
+};
 
 root.render(
   <ErrorBoundary>
     <React.StrictMode>
       <Provider store={store}>
         <BrowserRouter>
+          <StyledToastContainer />
           <App />
         </BrowserRouter>
       </Provider>
